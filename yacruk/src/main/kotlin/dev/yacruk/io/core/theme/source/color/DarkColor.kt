a89c6673bff179b2246ed59111210356b19127e4
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-06-13 21:28:33 +0300

    feat(YACRUK-1): Add YacrukColor

---
+++ yacruk/src/main/kotlin/dev/yacruk/io/core/theme/source/color/DarkColor.kt
@@ -0,0 +1,29 @@
+package dev.yacruk.io.core.theme.source.color
+
+import androidx.compose.ui.graphics.Color
+import dev.yacruk.io.core.theme.common.YacrukColor
+
+val renkon_beige = Color(0xFF969D7C)
+val rustling_leaves = Color(0xFF696E50)
+val black_mesa = Color(0xFF21260E)
+val bittersweet = Color(0xFFFCA24F)
+val skullcrusher_brass = Color(0xFFF1CA90)
+val sports_fan = Color(0xFFD9781E)
+val true_navy = Color(0xFF3E5577)
+val jambalaya = Color(0xFF674C33)
+val spice_of_life = Color(0xFF83483D)
+val stone_craft = Color(0xFF7B877A)
+val night_white = Color(0xFFE2E1DD)
+val ruined_smores = Color(0xFF100D17)
+
+val baseLightColorPalette =
+    YacrukColor(
+        primary = bittersweet,
+        onPrimary = ruined_smores,
+        secondary = black_mesa,
+        onSecondary = true_navy,
+        background = renkon_beige,
+        onBackground = ruined_smores,
+        surface = renkon_beige,
+        onSurface = ruined_smores,
+    )
