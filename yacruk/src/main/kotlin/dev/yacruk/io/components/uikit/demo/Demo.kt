ec81f0d0fc0aaaac149c28c4ece79e94c8d7a73c
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-07-04 00:09:12 +0300

    feat(YACRUK-15): Add Demo

---
+++ yacruk/src/main/kotlin/dev/yacruk/io/components/uikit/demo/Demo.kt
@@ -0,0 +1,10 @@
+package dev.yacruk.io.components.uikit.demo
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Demo(
+    modifier: Modifier
+) {
+}
