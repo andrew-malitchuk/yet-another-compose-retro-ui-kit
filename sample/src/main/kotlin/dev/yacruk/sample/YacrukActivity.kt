b6f5b3c891d11c15a93e58487afb3550972cf15e
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-07-21 18:58:45 +0300

    ref(YACRUK-15): Clean-up code

--- sample/src/main/kotlin/dev/yacruk/sample/YacrukActivity.kt
+++ sample/src/main/kotlin/dev/yacruk/sample/YacrukActivity.kt
@@ -1,59 +1,22 @@
 package dev.yacruk.sample
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Switch
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import dev.yacruk.io.R
-import dev.yacruk.io.components.uikit.badge.YacrukBadge
-import dev.yacruk.io.components.uikit.board.YacrukBorder
-import dev.yacruk.io.components.uikit.button.content.YacrukContentButton
-import dev.yacruk.io.components.uikit.button.icon.YacrukIconButton
-import dev.yacruk.io.components.uikit.button.ordinary.YacrukButton
-import dev.yacruk.io.components.uikit.checkbox.YacrukCheckbox
-import dev.yacruk.io.components.uikit.chip.YacrukChip
 import dev.yacruk.io.components.uikit.demo.Demo
-import dev.yacruk.io.components.uikit.field.YaaumBasicTextField
-import dev.yacruk.io.components.uikit.label.YacrukLabel
-import dev.yacruk.io.components.uikit.progress.YacrukProgressBar
-import dev.yacruk.io.components.uikit.slider.YacrukSlider
-import dev.yacruk.io.components.uikit.switch.YacrukSwitchButton
-import dev.yacruk.io.components.uikit.text.YacrukText
 import dev.yacruk.io.core.theme.common.YacrukTheme
 import dev.yacruk.io.core.theme.source.YacrukTheme
-import dev.yacruk.io.core.theme.source.color.black_mesa
-import dev.yacruk.io.core.theme.source.color.jambalaya
 import dev.yacruk.io.core.theme.source.color.renkon_beige
-import dev.yacruk.io.core.theme.source.color.rustling_leaves
-import dev.yacruk.io.core.theme.source.color.stone_craft
-import dev.yacruk.io.core.theme.source.color.true_navy
-import kotlinx.coroutines.launch
 
 class YacrukActivity : ComponentActivity() {
-    @OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
