526e436df207e807e23af0db7f71055d45e375ed
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-09-02 20:53:17 +0300

    ref(YACRUK-20): revert changesa

---
+++ yacruk/src/main/kotlin/dev/yacruk/io/core/theme/source/FontSize.kt
@@ -0,0 +1,15 @@
+package dev.yacruk.io.core.theme.source
+
+import androidx.compose.ui.unit.sp
+import dev.yacruk.io.core.theme.common.YacrukFontSize
+
+val fontSize =
+    YacrukFontSize(
+        display = 34.sp,
+        headline = 24.sp,
+        title = 20.sp,
+        subHeading = 16.sp,
+        body = 14.sp,
+        caption = 12.sp,
+        button = 14.sp,
+    )
